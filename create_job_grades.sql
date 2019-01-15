5676e62ac4de8e5321f789eee171bf7069615844
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-01-15 15:48:14 +0900

    job_grades表作成スクリプト

---
+++ create_job_grades.sql
@@ -0,0 +1,25 @@
+drop table job_grades
+/
+create table job_grades(
+grade_level varchar2(3) primary key,
+lowest_sal number,
+highest_sal number)
+/
+insert into job_grades
+values('A', 1000, 2999)
+/
+insert into job_grades
+values('B', 3000, 5999)
+/
+insert into job_grades
+values('C', 6000, 9999)
+/
+insert into job_grades
+values('D', 10000, 14999)
+/
+insert into job_grades
+values('E', 15000, 24999)
+/
+insert into job_grades
+values('F', 25000, 40000)
+/
